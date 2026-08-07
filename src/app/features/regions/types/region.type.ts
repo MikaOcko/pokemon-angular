767faa66e27f159a796833eb7647031ccf50911a
kimberly moreau <moreau.magali.kimberly@gmail.com>
2026-08-07 17:21:52 +0200

    feat: :sparkles: add locations type into region type

--- src/app/features/regions/types/region.type.ts
+++ src/app/features/regions/types/region.type.ts
@@ -2,3 +2,14 @@ export interface Region {
     name:string;
     url:string;
 }
+
+export interface RegionLocations{
+    id:number;
+    locations:Location[];
+    name:string;
+}
+
+export interface Location{
+    name:string;
+    url:string;
+}
