7988b89b887b74519051f685e5a44bb203771398
kimberly moreau <moreau.magali.kimberly@gmail.com>
2026-08-07 12:54:00 +0200

    chore: :truck: update app routes
    add an error page for unknown urls

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { ITEMS_PATHS, POKEMON_PATHS, REGIONS_PATHS, ROOT_PATHS } from './core/path.constants';
 import { Home } from './features/home/home';
+import { Regions } from './features/regions/regions';
+import { Pokemons } from './features/pokemons/pokemons';
+import { Items } from './features/items/items';
+import { PageNotFound } from './features/page-not-found/page-not-found';
 
 export const routes: Routes = [
     {
         path: ROOT_PATHS.base,
-        loadChildren: async () =>
-        import('./features/home/home').then((module) => module.HOME_ROUTES),
+        component: Home,
     },
     {
         path: 'home',
@@ -14,17 +17,18 @@ export const routes: Routes = [
     },
     {
         path: REGIONS_PATHS.base,
-        loadChildren: async () =>
-        import('./features/regions/regions').then((module) => module.HOME_ROUTES),
+        component: Regions,
     },
     {
         path: POKEMON_PATHS.base,
-        loadChildren: async () =>
-        import('./features/pokemons/pokemons').then((module) => module.HOME_ROUTES),
+        component: Pokemons,
     },
     {
         path: ITEMS_PATHS.base,
-        loadChildren: async () =>
-        import('./features/items/items').then((module) => module.HOME_ROUTES),
+        component: Items,
+    },
+    {
+        path: '**',
+        component: PageNotFound,
     },
 ];
