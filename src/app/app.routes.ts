8702e616a8b9f5f54a205fb3e5123b144c325d5a
kimberly moreau <moreau.magali.kimberly@gmail.com>
2026-08-07 12:24:20 +0200

    chore: :truck: add constants for path and url, update app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { ITEMS_PATHS, POKEMON_PATHS, REGIONS_PATHS, ROOT_PATHS } from './core/path.constants';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: ROOT_PATHS.base,
+        loadChildren: async () =>
+        import('./features/home/home').then((module) => module.HOME_ROUTES),
+    },
+    {
+        path: 'home',
+        component: Home,
+    },
+    {
+        path: REGIONS_PATHS.base,
+        loadChildren: async () =>
+        import('./features/regions/regions').then((module) => module.HOME_ROUTES),
+    },
+    {
+        path: POKEMON_PATHS.base,
+        loadChildren: async () =>
+        import('./features/pokemons/pokemons').then((module) => module.HOME_ROUTES),
+    },
+    {
+        path: ITEMS_PATHS.base,
+        loadChildren: async () =>
+        import('./features/items/items').then((module) => module.HOME_ROUTES),
+    },
+];
---
+++ src/app/core/path.constants.ts
@@ -0,0 +1,16 @@
+export const ROOT_PATHS = {
+  base: '',
+};
+export const REGIONS_PATHS = {
+  base: 'regions',
+};
+export const POKEMON_PATHS = {
+  base: 'pokemons',
+};
+export const ITEMS_PATHS = {
+  base: 'items',
+};
+export const ERROR_PATHS = {
+  base: 'error',
+  notFound: '404',
+};
