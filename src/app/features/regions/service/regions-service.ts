e83474c9bbe38a1e086815474749378407e7816b
kimberly moreau <moreau.magali.kimberly@gmail.com>
2026-08-07 15:25:42 +0200

    chore: :building_construction: update regions service

--- src/app/features/regions/service/regions-service.ts
+++ src/app/features/regions/service/regions-service.ts
@@ -1,4 +1,18 @@
-import { Service } from '@angular/core';
+import { inject, Service, signal } from '@angular/core';
+import { Region } from '../types/region.type';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Service()
-export class RegionsService {}
+export class RegionsService {
+    private http = inject(HttpClient);
+    private regions = signal<Region[]>([]);
+    readonly url = 'https://pokeapi.co/api/v2/region/';
+
+    // logique pour récupérer toutes les régions
+    getRegions():Observable<Region[]> {
+        return this.http.get<Region[]>(this.url).pipe(
+            tap(regions => this.regions.set(regions))
+        );
+    }
+}
